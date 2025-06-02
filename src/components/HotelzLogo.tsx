9e5b549ed699fa69433c4871fd6b6f2015af8323
Risiidhan <rkishon4@gmail.com>
2025-06-02 08:50:47 +0400

    initializing header component

---
+++ src/components/HotelzLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HotelzLogo = () => {
+  return (
+    <div className='flex items-center h-full'>
+      Logo
+    </div>
+  )
+}
+
+export default HotelzLogo
---
+++ src/layouts/header/right-panel/ProfileComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style.css';
+
+
+const ProfileComponent = ({children}: {children : React.ReactNode}) => {
+   return(
+     <div className='right-panel-container-btn'>
+           {children}
+    </div>
+   )
+
+}
+
+export default ProfileComponent
