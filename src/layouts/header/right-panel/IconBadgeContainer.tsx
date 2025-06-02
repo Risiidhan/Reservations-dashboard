36fc9eb4358436c242d2b8f362a810ce5c0669ae
Risiidhan <rkishon4@gmail.com>
2025-06-02 15:46:30 +0400

    header structure

--- src/layouts/header/right-panel/IconBadgeContainer.tsx
+++ src/layouts/header/right-panel/IconBadgeContainer.tsx
@@ -1,10 +1,22 @@
+import { BadgeType } from '@/enum/BadgeType';
 import React from 'react'
 
-const IconBadgeContainer = () => {
+const IconBadgeContainer = ({badgeType}: {badgeType: BadgeType}) => {
+
+  const renderIcon = () => {
+    switch (badgeType) {
+      case BadgeType.Alert:
+        return '🚗';
+      case BadgeType.Notification:
+        return '⚽️';  
+      default:
+        return null;
+  };
+  }
   return (
     <div className='right-panel-container-btn relative'>
       <div className='red-dot'></div>
-           {/* {children} */}
+           {renderIcon()}
     </div>
   )
 }
