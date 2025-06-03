0acc763bc560b939a061c9fe8e0b123f3b7d844a
Risiidhan <rkishon4@gmail.com>
2025-06-03 12:28:05 +0400

    sidemenu collapse

--- src/layouts/header/svg/NotificationSvg.tsx
+++ src/layouts/header/svg/NotificationSvg.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NotificationSvg = () => {
+const NotificationSvg: React.FC = () => {
   return (
   <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="21" height="16.9615" rx="4" fill="#BCBFC9"/>
