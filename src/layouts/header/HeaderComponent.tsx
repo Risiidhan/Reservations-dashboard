59b7453a132377e77957b1daaad9ecc014abb32b
Risiidhan <rkishon4@gmail.com>
2025-06-09 14:13:41 +0400

    make header sticky

--- src/layouts/header/HeaderComponent.tsx
+++ src/layouts/header/HeaderComponent.tsx
@@ -5,7 +5,7 @@ import SvgIcon from "@/components/SvgIcon";
 
 const HeaderComponent: React.FC = () => {
   return (
-    <div className="header-height flex-center bg-white border-b border-[#A6B4DA]">
+    <div className="header-height sticky top-0 flex-center bg-white border-b border-[#A6B4DA]">
       <div className="sidebar-width relative h-full flex-center-tablet border-r border-[#A6B4DA] px-4">
         <SvgIcon name={"hotelLogo"} width={151} height={46} />
         <CollapseSideMenu />
