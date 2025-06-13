5e8bee3450166333cfcdcd2fd2ddca8cb64d1d96
raffi23 <raffi@rhymecode.net>
2025-06-13 10:35:01 +0400

    add: tailwind, shadcn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,8 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
 export const isBrowser = () => typeof window !== "undefined";
